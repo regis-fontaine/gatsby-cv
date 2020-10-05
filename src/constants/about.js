4d6916372803758e3de3101dc42837aa34d746ed
Regis <sebastienhb@gmail.com>
2020-10-05 19:12:00 +0200

    .

--- src/constants/about.js
+++ src/constants/about.js
@@ -16,13 +16,11 @@ export default [
     ],
     image: "",
     info: [
-      "Bonjour, je suis Fontaine Régis, développeur web.",
-      "Récemment titulaire d'un titre professionnel de développeur web et web mobile, équivalent à un bac +2.",
-      "Et à la suite d'une première expérience en tant qu'intégrateur web, je souhaite aujourd'hui reprendre une formation afin de monter en compétences.",
-      "Je suis à la recherche d'une entreprise dans laquelle je pourrais progresser en tant que développeur front en alternance.",
-      "Mais aussi préparer le titre professionnel de concepteur développeur web (équivalent bac+3/4) avec la Wild Code School.",
-      "Le développement front est au cœur de ma passion pour le web, et plus temps passe, plus je m'intéresse à la création d'API, au déploiement continue, ou encore à l'architecture d'application.",
-      "Si votre entreprise souhaite d'un jeune passionné, dynamique, et sympa, n'hésitez pas à me contacter !",
+      "Bonjour, je m'appelle Régis Fontaine, et je suis développeur web à la recherche d'une alternance.",
+      "Je suis fraîchement diplômé un titre professionnel de Développeur Web et Web Mobile, équivalent à un bac +2.",
+      "À la suite d'une première expérience en tant qu'intégrateur web, je souhaite aujourd'hui reprendre une formation afin de monter en compétences.",
+      "Je suis à la recherche d'une entreprise en alternance dans laquelle je pourrais progresser en tant que développeur front-end. Mais aussi préparer le titre professionnel de Concepteur Développeur d'Application (équivalent bac+3/4) avec la Wild Code School.",
+      // "Le développement front est au cœur de ma passion pour le web, la création des composants UI, la mise en forme de la data, .. et la gestion de packages . Toutefois, je m'intéresse aussi à la création d'API, à la sécurité, au déploiement continu, et à l'architecture d'application.",
     ],
   },
 ]
